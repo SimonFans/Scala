f24e258eede3733970d342cf79608b798ef6ae6a
Simon Zhao <Ximengzhao1220@gmail.com>
2019-11-03 10:58:13 -0800

    Create Fundamental.scala

---
+++ Fundamental.scala
@@ -0,0 +1,112 @@
+// scala 构造器（一个主构造器和若干辅助构造器), 辅助构造器名称为this,每个辅助构造器都必须调用一个此前已经定义的辅助构造器或者主构造器
+
+//（1） 辅助构造器必须先调用主构造器
+/*
+class Counter{
+  private var value = 0
+  private var name =""
+  private var mode = 1
+  def this(name:String){  //定义辅助构造器，因为这是第一个辅助构造器，所以只能先调主
+    this()  // 调用主构造器
+    this.name=name
+  }
+  def this(name:String, mode:Int){ // 第二个辅助构造器
+    this(name)   // 调用第一个辅助构造器
+    this.mode=mode
+  }
+  def increment(step:Int)={
+    value+=step
+  }
+  def current():Int={value}
+  def Info={
+    printf("Name is %s, Mode is %d \n", name, mode)
+  }
+}
+
+object MyCounter{
+  def main(args: Array[String]): Unit = {
+    val counter1 = new Counter
+    counter1.Info
+    val counter2 = new Counter("Mark")
+    counter2.increment(3)
+    counter2.Info
+    printf("current value is %d ",counter2.current)
+  }
+}
+*/
+
+// (2) 直接定义参数在主构造器名称后面，不需要放在括号里
+/*
+class Counter(val name:String, val mode:Int){
+  private var value = 0
+  def increment(step:Int)={
+    value+=step
+  }
+  def current():Int={value}
+  def Info={
+    printf("Name is %s, and Mode is %d \n",name, mode)
+  }
+}
+
+object MyCounter{
+  def main(args: Array[String]): Unit = {
+    val myCounter = new Counter("Simon",1)
+    myCounter.Info
+    myCounter.increment(3)
+    printf("Current value is %d \n",myCounter.current)
+  }
+}
+*/
+
+/* 单例对象: no class keyword, object里的方法，变量都是静态的，相当于可以存值累加
+
+object Person{
+  private var lastId=0
+  def newPersonId()={
+    lastId+=1
+    lastId
+  }
+}
+
+object singleObject {
+  def main(args: Array[String]): Unit = {
+    printf("The first person Id is : %d. \n",Person.newPersonId())
+    printf("The second person Id is : %d. \n",Person.newPersonId())
+    printf("The third person Id is : %d. \n",Person.newPersonId())
+  }
+}
+*/
+
+// 伴生对象：class name and object name are same, must be in the same file, 方法相互调用。
+/*
+class Person{
+  private val id = Person.newPersonId()
+  private var name = ""
+  def this(name: String){
+    this()  // 调用主构造器
+    this.name=name
+  }
+  def Info: Unit ={
+    printf("The Id of %s is %d \n",name,id)
+  }
+}
+
+object Person{
+  private var lastId=0
+  def newPersonId() = {
+    lastId += 1
+    lastId
+  }
+  def main(args: Array[String]): Unit = {
+    val person1 = new Person("Ximeng")
+    person1.Info
+    val person2 = new Person("Simon")
+    person2.Info
+  }
+}
+*/
+
+
+
+
+
